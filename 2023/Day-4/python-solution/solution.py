cd8fdefc8f88390896e43cef73b0eacfb1926c72
absandell <asandell@ufl.edu>
2023-12-05 20:31:58 -0500

    Day 4, 2023 - Python

--- 2023/Day-4/python-solution/solution.py
+++ 2023/Day-4/python-solution/solution.py
@@ -1,75 +1,54 @@
 def get_winning_numbers(scratchcards):
+    cards = []
     final_sum = 0
     
-    for scratchcard in scratchcards.split('\n'):
-        card_part = scratchcard.split(":")
-        winning_numbers = card_part[1].split("|")[0]
-        possible_numbers = card_part[1].split("|")[1]
-
-        winning_number_set = {}
-        for winning_number in winning_numbers.split():
-            winning_number_set[winning_number] = 0
-        
-        for possible_number in possible_numbers.split():
-            if winning_number_set.get(possible_number, None) is not None: 
-                winning_number_set[possible_number] += 1
-            else: continue
-        
-        card_sum = 0
-        for number in winning_number_set:
-            if winning_number_set.get(number, None) is not None:
-                if winning_number_set.get(number, None) > 0:
-                    if card_sum == 0:
-                        card_sum += 1
-                    elif card_sum > 0:
-                        card_sum *= 2
-                else: continue
-            else: continue
-
-        final_sum += card_sum
-    print(final_sum)
+    for line in scratchcards.splitlines():
+        card = line.replace(':', '|').split('|')
+        winning_numbers = set(card[1].split())
+        possible_numbers = set(card[2].split())
+        card = [winning_numbers, possible_numbers]
+        cards.append(card)
+    
+    for c, card in enumerate(cards):
+        winning_numbers, possible_numbers = card
+        matches = winning_numbers & possible_numbers
+        num_matches = len(matches)
+        if num_matches == 0: continue
+        elif num_matches > 0:
+            card_sum = 0
+            for i in range(num_matches):
+                if card_sum == 0:
+                    card_sum += 1
+                elif card_sum > 0:
+                    card_sum *= 2
+            final_sum += card_sum
     return final_sum
 
 def get_count_cards(scratchcards):
-    final_sum = 0
-
-    extra_cards = {}
+    cards = []
     
-    for scratchcard in scratchcards.split('\n'):
-        card_part = scratchcard.split(":")
-        card_num = int(card_part[0].split()[1])
-        winning_numbers = card_part[1].split("|")[0]
-        possible_numbers = card_part[1].split("|")[1]
+    for line in scratchcards.splitlines():
+        card = line.replace(':', '|').split('|')
+        winning_numbers = set(card[1].split())
+        possible_numbers = set(card[2].split())
+        card = [1, winning_numbers, possible_numbers]
+        cards.append(card)
 
-        num_matched = 0
-        winning_number_set = {}
-        for winning_number in winning_numbers.split():
-            winning_number_set[winning_number] = 0
-        
-        for possible_number in possible_numbers.split():
-            if possible_number in winning_number_set:
-                winning_number_set[possible_number] += 1 
-                num_matched += 1
-        
-        
-        print("\nGame: ", card_num, "- Initial num matched is ", num_matched)
-        temp = card_num
-        if extra_cards.get(temp, None) is not None:
-            if num_matched > 0:
-                print("Had", extra_cards[card_num-1], "matches here")
-                num_matched +=  (num_matched * extra_cards[card_num])
-                print("Total matches after multiplying is ", num_matched)
-        
-        final_sum += num_matched + 1
-        print("Final Sum Here: ", final_sum)
+    for c, card in enumerate(cards):
+        count, winning_numbers, possible_numbers = card
+        matches = winning_numbers & possible_numbers
+        num_matches = len(matches)
+        if num_matches == 0: continue
+        elif num_matches > 0:
+            for num_extra in range(c+1, c+1+num_matches):
+                cards[num_extra][0] += count
+    return get_num_cards(cards)
 
-        for i in range (num_matched):
-            temp2 = card_num + i
-            extra_cards[temp2] = extra_cards.get(temp2, 0) + 1
-
-    print(final_sum)
+def get_num_cards(cards):
+    final_sum = 0
+    for card in cards:
+        final_sum += card[0]
     return final_sum
-
 
 def main():
     file_contents = ""
@@ -82,8 +61,8 @@ def main():
     except Exception as e:
         print(f"An error occurred: {e}")
 
-    get_winning_numbers(file_contents)
-    get_count_cards(file_contents)
+    print(get_winning_numbers(file_contents))
+    print(get_count_cards(file_contents))
 main()
 
 
